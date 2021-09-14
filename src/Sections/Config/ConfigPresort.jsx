5b2a1c531051adc201f02e04ce192d275f80f463
Shawn Banasick <banasick@gmail.com>
2021-09-14 17:50:03 +0900

    working on color pickers

--- src/Sections/Config/ConfigPresort.jsx
+++ src/Sections/Config/ConfigPresort.jsx
@@ -1,18 +1,19 @@
 import React, { useState } from "react";
 import { view } from "@risingstack/react-easy-state";
-import styled, { keyframes } from "styled-components";
-import GlobalStyle from "../../Utils/GlobalStyle";
-import GeneralButton from "../../Utils/GeneralButton";
-import exportToXml from "../../Utils/exportToXml";
-import RadioButtons from "../../Utils/RadioButtons";
-import UserTextInput from "../../Utils/UserTextInput";
-import Survey from "../Survey/Survey";
-import generateConfigXml from "../Config/generateConfigXml";
 import appState from "../../GlobalState/appState";
-import FadeIn from "./FadeIn";
-import UserNumberInput from "../../Utils/UserNumberInput";
-import { HexColorPicker, HexColorInput } from "react-colorful";
 import PopoverPicker from "./PopoverPicker";
+
+// import styled, { keyframes } from "styled-components";
+// import GlobalStyle from "../../Utils/GlobalStyle";
+// import GeneralButton from "../../Utils/GeneralButton";
+// import exportToXml from "../../Utils/exportToXml";
+// import RadioButtons from "../../Utils/RadioButtons";
+// import UserTextInput from "../../Utils/UserTextInput";
+// import Survey from "../Survey/Survey";
+// import generateConfigXml from "../Config/generateConfigXml";
+// import FadeIn from "./FadeIn";
+// import UserNumberInput from "../../Utils/UserNumberInput";
+// import { HexColorPicker, HexColorInput } from "react-colorful";
 
 const ConfigPresort = (props) => {
   const [color, setColor] = useState(props.default);
