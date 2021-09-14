5052f2b722f88cec4baf0ef449964e633b72ddd7
smb <smb>
2021-09-14 20:59:17 +0900

    styled color picker and added debouncy

--- src/Sections/Config/ConfigPresort.jsx
+++ src/Sections/Config/ConfigPresort.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { view } from "@risingstack/react-easy-state";
 import appState from "../../GlobalState/appState";
 import PopoverPicker from "./PopoverPicker";
+import { useDebouncyFn } from "use-debouncy";
 
 // import styled, { keyframes } from "styled-components";
 // import GlobalStyle from "../../Utils/GlobalStyle";
@@ -18,10 +19,11 @@ import PopoverPicker from "./PopoverPicker";
 const ConfigPresort = (props) => {
   const [color, setColor] = useState(props.default);
 
-  const handleOnChange = (e) => {
+  const handleOnChange = useDebouncyFn((e) => {
+    console.log(e);
     appState[props.card] = e;
     setColor(e);
-  };
+  }, 200);
 
   return <PopoverPicker color={color} onChange={handleOnChange} />;
 };
