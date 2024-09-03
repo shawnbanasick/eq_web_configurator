2bf144427acc4fcaf407a3574f8e2e2e8c1e8991
Shawn Banasick <banasick@mail.kobe-c.ac.jp>
2024-09-03 12:12:01 +0900

    fixed notary tool

--- scripts/notarize.js
+++ scripts/notarize.js
@@ -12,8 +12,10 @@ exports.default = async function notarizing(context) {
   console.log(process.env.APPLEID);
 
   return await notarize({
+    tool: "notarytool",
     appBundleId: "com.banasick.configurator",
     appPath: `${appOutDir}/${appName}.app`,
+    teamId: process.env.TEAMID,
     appleId: process.env.APPLEID,
     appleIdPassword: process.env.APPLEIDPASS,
   });
